ddfe76ecabfb75d51e4f0f757a7f514081c5da64
Amir Livneh <amir.livneh@primesense.com>
2013-05-23 20:37:02 +0300

    Use comtypes instead of win32com

--- doc2png.py
+++ doc2png.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import win32com.client
+import comtypes.client
 import subprocess
 
 wdFormatPDF = 17
@@ -9,7 +9,7 @@ in_file = os.path.abspath(sys.argv[1])
 pdf_file = os.path.splitext(in_file)[0] + ".pdf"
 png_file = os.path.splitext(in_file)[0] + "%02d.png"
 
-word = win32com.client.Dispatch('Word.Application')
+word = comtypes.client.CreateObject('Word.Application')
 doc = word.Documents.Open(in_file)
 doc.SaveAs(pdf_file, FileFormat=wdFormatPDF)
 doc.Close()
