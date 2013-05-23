71368ec17ebc4d93b26f3a068eaab69729384ad9
Amir Livneh <amir.livneh@primesense.com>
2013-05-23 19:34:23 +0300

    Initial working version

---
+++ doc2pdf.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import win32com.client
+import subprocess
+
+wdFormatPDF = 17
+
+in_file = os.path.abspath(sys.argv[1])
+pdf_file = os.path.splitext(in_file)[0] + ".pdf"
+png_file = os.path.splitext(in_file)[0] + "%02d.png"
+
+word = win32com.client.Dispatch('Word.Application')
+doc = word.Documents.Open(in_file)
+doc.SaveAs(pdf_file, FileFormat=wdFormatPDF)
+doc.Close()
+word.Quit()
+
+subprocess.call(["c:\Program Files (x86)\ImageMagick-6.8.5-Q16\convert", pdf_file, png_file])
