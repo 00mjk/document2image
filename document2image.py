80498c62f88d45fefa1435ce7f374968b0aaff76
Amir Livneh <amir.livneh@gmail.com>
2013-05-26 20:20:06 +0300

    Add main function

--- document2image.py
+++ document2image.py
@@ -15,21 +15,25 @@ def get_imagemagick_bin_path():
                                                 "SOFTWARE\ImageMagick\Current"),
                               "BinPath")[0]
 
-in_file = os.path.abspath(sys.argv[1])
-png_file = os.path.splitext(in_file)[0] + "%02d.png"
+def main():
+    in_file = os.path.abspath(sys.argv[1])
+    png_file = os.path.splitext(in_file)[0] + "%02d.png"
+    
+    if in_file.endswith(".doc"):
+        word = comtypes.client.CreateObject('Word.Application')
+        doc = word.Documents.Open(in_file)
+        pdf_file = os.path.splitext(in_file)[0] + ".pdf"
+        doc.SaveAs(pdf_file, FileFormat=wdFormatPDF)
+        doc.Close()
+        word.Quit()
+    elif in_file.endswith(".pdf"):
+        pdf_file = in_file
+    else:
+        sys.stderr.write("Unsupported file type: %s" % os.path.splitext(in_file)[1])
+        sys.exit(-1)
+    
+    subprocess.call([os.path.join(get_imagemagick_bin_path(), "convert"),
+                     "-density", "150", pdf_file, png_file])
 
-if in_file.endswith(".doc"):
-    word = comtypes.client.CreateObject('Word.Application')
-    doc = word.Documents.Open(in_file)
-    pdf_file = os.path.splitext(in_file)[0] + ".pdf"
-    doc.SaveAs(pdf_file, FileFormat=wdFormatPDF)
-    doc.Close()
-    word.Quit()
-elif in_file.endswith(".pdf"):
-    pdf_file = in_file
-else:
-    sys.stderr.write("Unsupported file type: %s" % os.path.splitext(in_file)[1])
-    sys.exit(-1)
-
-subprocess.call([os.path.join(get_imagemagick_bin_path(), "convert"),
-                 "-density", "150", pdf_file, png_file])
+if __name__ == '__main__':
+    main()
